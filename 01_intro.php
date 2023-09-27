40744138582669cd67358fadcd8ee13e4c58e643
unknown <CIT173B-11@S312.UC-BCF.EDU.PH>
2023-09-27 17:54:25 +0800

    first commit

---
+++ 01_intro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to PHP</title>
+</head>
+<body>
+    <?php
+    echo "Hello, World!";
+    echo "<br>Hello, World!";
+    ?>
+
+</body>
+</html>
