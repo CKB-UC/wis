3b125ef23edcdba01c8897d00cd6b81c193bdc48
unknown <baloskeanu@gmail.com>
2023-10-04 18:09:15 +0800

    Updating Constants

--- 05_constants.php
+++ 05_constants.php
@@ -1,5 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-define("MINSIZE", 12);
+define("MINSIZE", 50);
+define("ONE", 1);
+define("TWO2", 2);
+define("THREE_3", 3);
+
 echo MINSIZE;
+echo ("<br>");
 echo constant("MINSIZE"); // same thing as the previous line
+echo ("<br>");
+echo constant("ONE");
+echo ("<br>");
+echo constant("TWO2");
+echo ("<br>");
+echo constant("THREE_3");
 ?>
+</body>
+</html>
