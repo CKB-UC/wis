d50a78c0698e0f7480c2a83e21cec8fc4143fda5
unknown <baloskeanu@gmail.com>
2023-10-04 17:59:46 +0800

    Trying variables

--- 04_variable_types.php
+++ 04_variable_types.php
@@ -1,4 +1,38 @@
-<?php
-echo "Live Server";
-echo "<br>Ayun gumana";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php    
+$x = "Hello world!";
+$y = 'Hello world!';
+
+    echo $x;
+    echo "<br>";
+    echo $y;
+
+$i = 5;
+$j = 10;
+
+    echo "<br>";
+    echo $i + $j;
+
+
+    
+// multiply a value by 10 and return it to the caller
+function multiply ($value) {
+ $value = $value * 10;
+ return $value;
+}
+$retval = multiply (10);
+echo "<br>";
+Print "Return value is $retval\n";
+
 ?>
+
+</body>
+</html>
