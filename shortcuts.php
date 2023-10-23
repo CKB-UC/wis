a8df060270516741276b36f366211b74d616426e
unknown <baloskeanu@gmail.com>
2023-10-23 17:53:35 +0800

    additional Shorcuts

--- shortcuts.php
+++ shortcuts.php
@@ -4,12 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VS Shortcuts Testings</title>
+<style>
+    table, th, td{
+        border: 1px solid black;
+    }
+</style>
+
 </head>
 <body>
-<center><h1>Visual Studio Code Shortcuts<H1></center>
+<center><h1>Shortcuts for Hands on Exams<h1></center>
     <hr>
 <br>
-<h3>General</h3>
+<h3>Basics</h3>
+<table>
+    <tr>
+        <th>Ctrl+`</th> <td>Open Terminal</td>  
+    </tr>
+    <tr>
+        <th>Ctrl+s</th> <td>Save File</td>
+    </tr>
+    <tr>
+        <th>Ctrl+P</th> <td>Quick Open, Go to File</td>
+    </tr>
+    <tr>
+        <th>Ctrl+Shift+N</th> <td>New Window</td>
+    </tr>
+    <tr>
+        <th>Ctrl+Home</th> <td>Go to Beginning of Line</td>
+    </tr>
+    <tr>
+        <th>Ctrl+End</th> <td>Go to End of Line</td>
+    </tr>
+    <tr>
+        <th>Ctrl+tab</th> <td>move to currently opened file </td>
+    </tr>
+    <tr>
+        <th>Windows+E</th> <td>To run File Explorer </td>
+    </tr>
+    <tr>
+        <th>Windows+R</th>  <td>Open RUN Box</td>
+    </tr>
+
+</table>
 
 </body>
 </html>
