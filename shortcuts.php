6eaf0c71aa9953aa072b6de75afef2199bf00f9b
unknown <baloskeanu@gmail.com>
2023-10-23 17:35:36 +0800

    updated

---
+++ shortcuts.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VS Shortcuts Testings</title>
+</head>
+<body>
+<center><h1>Visual Studio Code Shortcuts<H1></center>
+    <hr>
+<br>
+<h3>General</h3>
+
+</body>
+</html>
