9b20bcc41171a2b057086d0108f2c2cd7db96fb7
unknown <baloskeanu@gmail.com>
2023-12-10 21:36:27 +0800

    change name

--- setup.php
+++ setup.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $servername = "localhost";
 $username = "root";
@@ -82,3 +90,5 @@ else
     echo "Error: " . $sql3 . "<br>" . $conn->error;
     }
 ?>
+</body>
+</html>
